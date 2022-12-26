5ad9fa59fa9a037d25b1ea90be91cb0d955b46e5
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 19:50:57 +0530

    counts.

---
+++ counts/js/count.js
@@ -0,0 +1,14 @@
+// var string = prompt("enter string");
+// var word = prompt("enter string");
+// var temp = string.split(" ");
+// var count = 0;
+// for(i=0;i<temp.length;i++)
+// {
+    // if(temp[i]==word)
+    // {
+		
+        // count++;
+    // }
+// }
+// document.write(count);
+
